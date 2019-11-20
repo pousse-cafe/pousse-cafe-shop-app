143f32e63e73264a41bd4b9c997730a77063c61c
Gerard Dethier <g.dethier@gmail.com>
2019-11-20 10:04:26 +0100

    Use new way of configuring PC.

--- src/main/java/poussecafe/shop/AppConfiguration.java
+++ src/main/java/poussecafe/shop/AppConfiguration.java
@@ -1,33 +1,31 @@
 package poussecafe.shop;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import poussecafe.environment.Bundle;
 import poussecafe.journal.Journal;
 import poussecafe.messaging.internal.InternalMessaging;
+import poussecafe.runtime.Bundles;
 import poussecafe.runtime.MessagingAndStorage;
-import poussecafe.spring.RuntimeConfiguration;
 import poussecafe.spring.mongo.storage.SpringMongoDbStorage;
 
 @Configuration
 @ComponentScan(basePackages = { "poussecafe.spring" })
-public class AppConfiguration extends RuntimeConfiguration {
+public class AppConfiguration {
 
-    @Override
-    protected List<Bundle> bundles() {
-        MessagingAndStorage messagingAndStorage = new MessagingAndStorage(InternalMessaging.instance(),
-                SpringMongoDbStorage.instance());
-        List<Bundle> bundles = new ArrayList<>();
-        bundles.add(Journal.configure()
+    @Bean
+    protected Bundles bundles(InternalMessaging messaging,
+            SpringMongoDbStorage storage) {
+        MessagingAndStorage messagingAndStorage = new MessagingAndStorage(messaging, storage);
+        return new Bundles.Builder()
+            .withBundle(Journal.configure()
                 .defineThenImplement()
                 .messagingAndStorage(messagingAndStorage)
-                .build());
-        bundles.add(Shop.configure()
+                .build())
+            .withBundle(Shop.configure()
                 .defineThenImplement()
                 .messagingAndStorage(messagingAndStorage)
-                .build());
-        return bundles;
+                .build())
+            .build();
     }
 }
