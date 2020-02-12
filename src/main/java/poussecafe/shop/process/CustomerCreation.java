535bd0fae257f7abee1178034bb42cbde156d1b5
Gerard Dethier <g.dethier@gmail.com>
2020-02-12 10:43:09 +0100

    Use Module annotation on processes.

--- src/main/java/poussecafe/shop/process/CustomerCreation.java
+++ src/main/java/poussecafe/shop/process/CustomerCreation.java
@@ -1,7 +1,10 @@
 package poussecafe.shop.process;
 
+import poussecafe.discovery.Module;
 import poussecafe.domain.Process;
+import poussecafe.shop.Shop;
 
+@Module(Shop.class)
 public interface CustomerCreation extends Process {
 
 }
